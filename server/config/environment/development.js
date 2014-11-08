7872bc82651fe1561d1dc8ee109bdc9d618029f6
rankizi <rankizi@gmail.com>
2014-11-08 17:03:21 +0200

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/onlinemarketer-dev'
+  },
+
+  seedDB: true
+};
